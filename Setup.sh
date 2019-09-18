f863891bea41782b9be60eea71a3ce7209ec707d
STODMAIN\tyron.smith <tyron.smith@stoddarts.com.au>
2019-09-18 15:27:25 +1000

    Added the setup file.

---
+++ Setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker build -t laravel:latest .
+
+docker run --name $CONTAINER -v $SOURCELOC:/var/www/html -d -p 80:80 laravel 
+
+# run composer install on ContractPlus Web
+docker exec -u 0 $CONTAINER composer install -d /var/www/html/project
+docker exec -u 0 $CONTAINER composer update -d /var/www/html/project
+
+# install npm on ContractPlus Web and run the manager
+docker exec -u 0 $CONTAINER npm --prefix /var/www/html/project install /var/www/html/project
+docker exec -u 0 $CONTAINER npm --prefix /var/www/html/project run dev /var/www/html/project
