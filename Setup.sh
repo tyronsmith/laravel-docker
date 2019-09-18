88eb6eed027b506f5847388a1219dadc83866bc3
STODMAIN\tyron.smith <tyron.smith@stoddarts.com.au>
2019-09-18 15:29:13 +1000

    Fixed some comments.

--- Setup.sh
+++ Setup.sh
@@ -4,10 +4,10 @@ docker build -t laravel:latest .
 
 docker run --name $CONTAINER -v $SOURCELOC:/var/www/html -d -p 80:80 laravel 
 
-# run composer install on ContractPlus Web
+# run composer install on project folder
 docker exec -u 0 $CONTAINER composer install -d /var/www/html/project
 docker exec -u 0 $CONTAINER composer update -d /var/www/html/project
 
-# install npm on ContractPlus Web and run the manager
+# install npm on project folder and run the manager
 docker exec -u 0 $CONTAINER npm --prefix /var/www/html/project install /var/www/html/project
 docker exec -u 0 $CONTAINER npm --prefix /var/www/html/project run dev /var/www/html/project
