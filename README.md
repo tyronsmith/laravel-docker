d7d826e1c72a6e73be1f29f7c7355761f2a85671
STODMAIN\tyron.smith <tyron.smith@stoddarts.com.au>
2019-09-19 07:57:58 +1000

    Changed the Setup

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Docker Environment for Laravel
 Built on Centos running;
 * Apache
 * PHP 7.2
+
+To Run
+Execute the Setup.sh file to build the docker image and then start the container.
+This script expects 3 variables;
+	* Setup.sh contname="<containerName>" project="<yourProjectName>" repo="<locationOfYourRepoWithProject" 
+	
+This execution will;
+	* Mount the repo location to the container.
+	* Run composer install on your project folder in the container.
+	* Run npm install on your project folder in the container.
--- Setup.sh
+++ Setup.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+for ARGUMENT in "$@"
+do
+
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+
+    case "$KEY" in
+            contname) CONTNAME=${VALUE} ;;
+            repo) REPOSITORY=${VALUE} ;;     
+			project) PROJNAME=${VALUE} ;; 
+            *)   
+    esac    
+
+
+done
+
 docker build -t laravel:latest .
 
-docker run --name $CONTAINER -v $SOURCELOC:/var/www/html -d -p 80:80 laravel 
+docker run --name $CONTNAME -v $REPOSITORY:/var/www/html -d -p 80:80 laravel 
 
 # run composer install on project folder
-docker exec -u 0 $CONTAINER composer install -d /var/www/html/project
-docker exec -u 0 $CONTAINER composer update -d /var/www/html/project
+docker exec -u 0 $CONTNAME composer install -d /var/www/html/$PROJNAME
+docker exec -u 0 $CONTNAME composer update -d /var/www/html/$PROJNAME
 
 # install npm on project folder and run the manager
-docker exec -u 0 $CONTAINER npm --prefix /var/www/html/project install /var/www/html/project
-docker exec -u 0 $CONTAINER npm --prefix /var/www/html/project run dev /var/www/html/project
+docker exec -u 0 $CONTNAME npm --prefix /var/www/html/$PROJNAME install /var/www/html/$PROJNAME
+docker exec -u 0 $CONTNAME npm --prefix /var/www/html/$PROJNAME run dev /var/www/html/$PROJNAME
