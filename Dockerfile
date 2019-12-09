573abb2793a3d6578762858d7f53ee407487d1a7
tyronsmith <tyron.smith@informed365.com>
2019-12-09 21:34:46 +1000

    Adding information for the postgres docker container.

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm && \
 RUN yum -y update
 
 # php && httpd
-RUN yum -y install php httpd php-cli php-pgsql php-pdo php-ldap php-intl php-zip php-xml php-mbstring ca-certificates gcc-c++ make libpng build-essential libpng-devel ghostscript php-bcmath redis
+RUN yum -y install php httpd php-cli php-pgsql php-pdo php-ldap php-intl php-zip php-xml php-mbstring ca-certificates gcc-c++ make libpng build-essential libpng-devel ghostscript php-bcmath redis php-nrk-Predis
 
 # tools
 RUN yum -y install yum-utils epel-release iproute at curl crontabs
@@ -26,10 +26,13 @@ RUN yum -y install nodejs
 # we want some config changes
 COPY config/v-host.conf /etc/httpd/conf.d/z-host.conf
 
+COPY config/informed_365_db_prod_20191205_1610.dump /home/informed365.dump
+#RUN psql informed_365_db < /home/informed365.dump
+
 # create webserver-default directory
 RUN mkdir -p /var/www/html
 	
-EXPOSE 80 81
+EXPOSE 80 81 6379
 
 RUN systemctl enable httpd \
  && systemctl enable crond 
--- FirstRun.sh
+++ FirstRun.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 docker build . -t laravel:latest
-docker run --name informed -v c:/git:/var/www/html -d -p 80:80 laravel
+docker run --name informed -v d:/git/laravel-api:/var/www/html/project -d -p 80:80 laravel
 docker exec -u 0 informed composer install -d /var/www/html/project
 docker exec -u 0 informed composer update -d /var/www/html/project
 docker exec -u 0 informed php /var/www/html/project/artisan migrate
+
+docker run --name informed-postgres -e POSTGRES_PASSWORD= -d postgres
+docker cp d:/git/informed_365_db_prod_20191205_1610.dump informed-postgres:/home/informed365.dump
+#docker exec informed-postgres -u postgres createuser informed365_db_user
+docker exec -u postgres informed-postgres psql -c 'create user informed365_db_user'
+docker exec -u postgres informed-postgres psql -c 'CREATE DATABASE informed_365_db;'
+docker exec informed-postgres pg_restore -U postgres -d informed_365_db -n informed_365_db /home/informed365.dump
+
 
 # install npm on ContractPlus Web and run the manager
 docker exec -u 0 devenv npm --prefix /var/www/html/contractplus-web install /var/www/html/contractplus-web
@@ -13,4 +21,10 @@ docker exec -u 0 devenv update-ca-trust force-enable
 docker cp c:/users/tyrons/docker/devweb/config/Stoddart_Group_Issuing_CA.cer devenv:/etc/pki/ca-trust/source/anchors/
 docker cp c:/users/tyrons/docker/devweb/config/Stoddart_Group_Offline_Root_CA.cer devenv:/etc/pki/ca-trust/source/anchors/
 docker exec -u 0 devenv update-ca-trust extract
+
+docker exec -u 0 informed pg_restore -U postgres -d informed_365_db /home/informed365.dump
+
+
+
+docker exec -u postgres informed-postgres psql -d informed_365_db -c 'SELECT * FROM pg_catalog.pg_tables'
 
